5ae55d00150280c020b58d511ca5bc17db7dfe94
dinomoon <ansrud45@gmail.com>
2025-01-06 23:17:34 +0900

    Fix SearchBar, Add components folder

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,17 +1,23 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function SearchBar() {
+  const router = useRouter();
   const [search, setSearch] = useState('');
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const onClick = () => {
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
       <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <button onClick={onClick}>검색</button>
     </div>
   );
 }
